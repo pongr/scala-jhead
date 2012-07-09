313cb603aaf2bee6dce4eda3f7a4ee3576c6a99e
Zach Cox <zcox522@gmail.com>
2012-07-09 11:20:58 -0500

    notes on new image methods

--- src/main/scala/jhead.scala
+++ src/main/scala/jhead.scala
@@ -91,6 +91,17 @@ class JHead(file: File) {
     val result = exec("jhead", "-v", "-autorot", "-purejpg", file.getAbsolutePath)
     (ImageInfo(result._1), result._2, getBytes)
   }
+  
+  /** Resize image to be maximum width of specified value, height adjusted to keep aspect ratio constant. Specified width must be greater than image width. 
+    * Returns resized image and new height.
+    */
+  def downsizeToWidth(width: Int): (Array[Byte], Int) = null
+  
+  
+  /** Resize image to be maximum of specified size on each side. If image is portrait then remove top & bottom. If image is landsape then remove left & right. 
+    * Specified size must be greater than image width and height. Returns resized image.
+    */
+  def downsizeToSquare(size: Int): Array[Byte] = null
 
 
   /**
@@ -102,6 +113,7 @@ class JHead(file: File) {
     val thumbFile = File.createTempFile("thumb-%sx%s-" format (width, height), ".jpg")
     exec("convert", "-thumbnail", thumbGeometry,
                     "-gravity", "center",
+                    "-quality", "90",
                     "-crop", cropGeometry,
                     "-interpolate", "bicubic",
                     "+repage",
